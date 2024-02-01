e37f1d7f3faf2cde7239e1833e6c51f6200ea936
reshenieZadachek <158518714+reshenieZadachek@users.noreply.github.com>
2024-02-01 23:50:25 +0300

    Update connect.php

--- 3/connect.php
+++ 3/connect.php
@@ -1,5 +1,5 @@
 <?php//соединение с бд
-$connect = mysqli_connect('localhost', 'root', '', 'comments');
+$connect = mysqli_connect('$hostname', '$username', '$password', '$database');
 
 if (!$connect) {
     die('Error connect to DataBase');
